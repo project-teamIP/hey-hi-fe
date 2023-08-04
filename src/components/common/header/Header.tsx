d0c1c87ae6234575f05da7861ff0b0f182c99de6
solightnsalt <>
2023-08-04 18:19:18 +0900

    Feat: Header 극초기 레이아웃

--- src/components/common/header/Header.tsx
+++ src/components/common/header/Header.tsx
@@ -1,7 +1,34 @@
-import React from "react";
+import Button from "../button/Button";
+import * as S from "./style";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <S.HeaderBox>
+      <S.HeaderInner>
+        <S.Nav>
+          <h1>
+            <S.StyledLink to="/">LOGO</S.StyledLink>
+          </h1>
+          <ul>
+            <li>
+              <S.StyledLink to="/dashboard">home</S.StyledLink>
+            </li>
+            <li>
+              <S.StyledLink to="/">FAQ</S.StyledLink>
+            </li>
+            <li>
+              <S.StyledLink to="/">뭐넣지</S.StyledLink>
+            </li>
+          </ul>
+        </S.Nav>
+        <S.StyledLink to="/login">
+          <Button.Primary size="loginbtn" outlined>
+            로그인 / 회원가입
+          </Button.Primary>
+        </S.StyledLink>
+      </S.HeaderInner>
+    </S.HeaderBox>
+  );
 };
 
 export default Header;
