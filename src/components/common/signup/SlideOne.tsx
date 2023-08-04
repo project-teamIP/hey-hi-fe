d530bd3fc89e6d5dfbef51197ffca68c45e3f77a
2Zerozero <hawgae@gmail.com>
2023-08-04 22:41:29 +0900

    🎨 design: 회원가입 UI 구성중

--- src/components/common/signup/SlideOne.tsx
+++ src/components/common/signup/SlideOne.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import * as S from "./style";
+import Button from "../button/Button";
+import Input from "../input/Input";
+import Select from "../select/Select";
+
 const SlideOne = () => {
   return (
     <S.Wrap>
-      <div>1</div>
+      <div>닉네임</div>
+      <div>
+        <Input placeholder="닉네임 입력" value="value" />
+        <Button.Primary size="small">1</Button.Primary>
+      </div>
+      <div>거주국가</div>
+      <Select label="국가를 선택해주세요." options={["한국", "일본"]} />
     </S.Wrap>
   );
 };
