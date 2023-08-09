f4fb44e904f4dab03bf70d0a6a5cd61c97974591
solightnsalt <>
2023-08-10 08:14:50 +0900

    ✨ feat: main review slide 뼈대

--- src/components/main/mainReview/MainReview.tsx
+++ src/components/main/mainReview/MainReview.tsx
@@ -1,5 +1,5 @@
-import React from "react";
 import * as S from "./style";
+import Reviews from "./Reviews";
 
 const MainReview = () => {
   return (
@@ -8,10 +8,7 @@ const MainReview = () => {
       <h1>생생한 유저들의 후기!</h1>
       <p>계속 한줄로 텍스트를 입력해 문장을 완성해주세요.</p>
       <S.ReviewWrapper>
-        <img src="https://via.placeholder.com/376x491" alt="review1" />
-        <img src="https://via.placeholder.com/376x491" alt="review2" />
-        <img src="https://via.placeholder.com/376x491" alt="review3" />
-        <img src="https://via.placeholder.com/376x491" alt="review4" />
+        <Reviews />
       </S.ReviewWrapper>
     </S.MainReviewBox>
   );
