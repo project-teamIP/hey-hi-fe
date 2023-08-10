fe7f3d7d01dd822d66843f93a987f0af9271c5f5
solightnsalt <>
2023-08-10 12:22:22 +0900

    ✨ feat: main review slide

--- src/components/main/mainReview/style.ts
+++ src/components/main/mainReview/style.ts
@@ -15,12 +15,12 @@ export const MainReviewBox = styled.div`
     line-height: 53px;
     margin-top: 21px;
   }
+`;
 
-  p {
-    font-size: 20px;
-    font-weight: 400;
-    margin: 20px 0 102px 0;
-  }
+export const ReviewSubtitle = styled.p`
+  font-size: 20px;
+  font-weight: 400;
+  margin: 20px 0 102px 0;
 `;
 
 export const SectionNum = styled.div`
@@ -36,7 +36,57 @@ export const SectionNum = styled.div`
 `;
 
 export const ReviewWrapper = styled.div`
-  img:not(:last-child) {
-    margin-right: 40px;
-  }
+  margin-top: 102px;
+`;
+
+export const Cards = styled.div`
+  width: 1920px;
+`;
+
+export const ReviewCard = styled.div`
+  width: 474px !important;
+  height: 251px;
+  border-radius: 29px;
+  background-color: ${colors.gray};
+  display: flex !important;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 25px;
+`;
+
+export const CardContent = styled.div`
+  width: 272px;
+  height: 195px;
+  margin-top: 28px;
+  position: relative;
+`;
+
+export const Purpose = styled.p`
+  width: max-content;
+  height: 29px;
+  padding: 0 10px;
+  background-color: ${colors.blue};
+  color: ${colors.gray};
+  border-radius: 16px;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 32px;
+  text-align: center;
+  margin-bottom: 13px;
+`;
+
+export const Content = styled.p`
+  font-size: 16px;
+  font-weight: 700;
+  line-height: 20px;
+  width: 236px;
+`;
+
+export const UserInfo = styled.p`
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 20px;
+  color: #959595;
+  position: absolute;
+  bottom: 0;
 `;
