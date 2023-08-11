cf4240ad7867460adac7b352f39bb169f5c0d0fb
2Zerozero <hawgae@gmail.com>
2023-08-12 04:04:19 +0900

    ✨ feat: 스크롤 전역으로 제거

--- src/assets/styles/GlobalStyle.tsx
+++ src/assets/styles/GlobalStyle.tsx
@@ -8,6 +8,12 @@ const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     font-family: 'Pretendard', sans-serif;
+    -ms-overflow-style: none; /* 인터넷 익스플로러 */
+    scrollbar-width: none; /* 파이어폭스 */
+
+    &::-webkit-scrollbar {
+      display: none; /* 크롬, 사파리, 오페라, 엣지 */
+    }
   }
 `;
 
