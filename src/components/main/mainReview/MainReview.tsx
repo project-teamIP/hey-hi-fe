355e15ef8f420583b2a4b132676b5fc00b3c6c0e
solightnsalt <>
2023-08-11 23:10:25 +0900

    🐛 fix: 불필요한import삭제

--- src/components/main/mainReview/MainReview.tsx
+++ src/components/main/mainReview/MainReview.tsx
@@ -3,7 +3,6 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import reviews from "../../../utils/reviews.json";
-import { CardContent } from "./style";
 
 const MainReview = () => {
   const settings = {
