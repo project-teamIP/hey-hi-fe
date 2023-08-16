5b1d4effb16b475dfa46578a7e08953be2fea9f9
solightnsalt <>
2023-08-17 01:55:04 +0900

    ✨feat: memo 클릭 시 modal

--- src/utils/formattedDate.ts
+++ src/utils/formattedDate.ts
@@ -10,3 +10,16 @@ export function formatDateTime(dateTime: string): string {
 
   return `${year}.${month}.${day} ${hours}:${minutes}`;
 }
+
+// createdAt: '2023-08-10T10:40:20.275486' -> 2023년 8월 10일 10시 40분
+export function formatDateTime2(dateTime: string): string {
+  const createdAtDate = new Date(dateTime);
+
+  const year = createdAtDate.getFullYear();
+  const month = String(createdAtDate.getMonth() + 1).padStart(2, "0");
+  const day = String(createdAtDate.getDate()).padStart(2, "0");
+  const hours = String(createdAtDate.getHours()).padStart(2, "0");
+  const minutes = String(createdAtDate.getMinutes()).padStart(2, "0");
+
+  return `${year}년 ${month}월 ${day}일 ${hours}시 ${minutes}분`;
+}
