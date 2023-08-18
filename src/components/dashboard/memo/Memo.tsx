7dabc8cd142142dfe4eb85ab941f37027d0a8316
HyerimNac:/Users/나혜림/OneDrive/바탕 화면/test/venv/Scripts/python.exe <rimhyena@gmail.com>
2023-08-19 03:43:24 +0900

    ✨feat:dashboard_CallLog api 연결+ 대시보드 일부 스타일 정리

--- src/components/dashboard/memo/Memo.tsx
+++ src/components/dashboard/memo/Memo.tsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import instance from "../../../api/api";
 import { useQuery } from "react-query";
 import { DashboardMemosType } from "../../../types/user";
+import * as S from "./style";
 
 const Memo = () => {
   const [memoData, setMemoData] = useState<DashboardMemosType[]>([]);
@@ -17,7 +18,7 @@ const Memo = () => {
 
   console.log("memoData", memoData);
   // 대시보드 정보 조회
-  const getDashboardInfo = async () => {
+  const getDashboardMemoInfo = async () => {
     try {
       const response = await instance.get(`/api/users/dashboard`);
       const memoData = response.data.memos;
@@ -28,7 +29,7 @@ const Memo = () => {
   };
 
   useEffect(() => {
-    getDashboardInfo();
+    getDashboardMemoInfo();
   }, []); // 컴포넌트 마운트 시에만 대시보드 정보 조회
 
   useEffect(() => {
@@ -46,27 +47,27 @@ const Memo = () => {
 
   return (
     <DashBoardBox size="memo">
-      <TextContainer>
-        <WordGroup>
+      <S.TextContainer>
+        <S.WordGroup>
           <h3>내가 쓴 메모</h3>
           <p>더보기</p>
-        </WordGroup>
-        <TextBoxGroup>
+        </S.WordGroup>
+        <S.TextBoxGroup>
           {memoData.length > 0 ? (
             memoData.map((memo, index) => {
               if (index < 4) {
                 return (
-                  <TextBox key={memo.id}>
+                  <S.TextBox key={memo.id}>
                     <div>
-                      <TextBoxInfo>
+                      <S.TextBoxInfo>
                         <p>{memo.date}</p>
-                        <TextBoxTag>{memo.nickname}과의 통화</TextBoxTag>
-                      </TextBoxInfo>
+                        <S.TextBoxTag>{memo.nickname}과의 통화</S.TextBoxTag>
+                      </S.TextBoxInfo>
                       <h2>{memo.title}</h2>
                       <p>{memo.content}</p>
                       {/* <p>Index: {index}</p> */}
                     </div>
-                  </TextBox>
+                  </S.TextBox>
                 );
               }
               return null; // 3보다 큰 인덱스는 렌더링하지 않음
@@ -74,94 +75,10 @@ const Memo = () => {
           ) : (
             <p>메모 데이터가 없습니다.</p>
           )}
-        </TextBoxGroup>
-      </TextContainer>
+        </S.TextBoxGroup>
+      </S.TextContainer>
     </DashBoardBox>
   );
 };
-
-const TextBox = styled.div`
-  background-color: #f8f9fc;
-  margin-top: 30px;
-  /* margin: 30px; */
-  width: 249px;
-  height: 257px;
-  flex-shrink: 0;
-  padding: 24px 22px 24px 22px;
-  border-radius: 20px;
-  border: 1px solid #d8dee9;
-
-  h2 {
-    color: #3f3f3f;
-    font-size: 20px;
-    font-weight: 700;
-    line-height: normal;
-    margin-bottom: 16px;
-  }
-
-  p {
-    color: #8c8c8c;
-    font-size: 15px;
-    font-weight: 500;
-    line-height: normal;
-  }
-`;
-
-const TextBoxTag = styled.div`
-  padding-top: 5px;
-  text-align: center;
-  border-radius: 30px;
-  width: 109px;
-  height: 24px;
-  flex-shrink: 0;
-  background-color: #ffe4dc;
-  color: #ff6e46;
-  font-size: 10px;
-  font-weight: 600;
-  line-height: normal;
-`;
-
-const TextBoxInfo = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  margin-bottom: 27px;
-
-  p {
-    color: #939393;
-    font-size: 13px;
-    font-weight: 600;
-    line-height: normal;
-  }
-`;
-
-const TextContainer = styled.div`
-  margin: 43px 23px 36px 23px;
-  overflow: hidden;
-`;
-
-const TextBoxGroup = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 20px;
-  background-color: blue;
-`;
-
-const WordGroup = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  h3 {
-    font-size: 20px;
-    font-weight: 600;
-    line-height: 23.87px;
-  }
-  p {
-    font-size: 13px;
-    font-weight: 600;
-    color: #b5b5b5;
-    line-height: 15.51px;
-  }
-`;
 
 export default Memo;
