9ebfc1a980f9370b2c7740fe1efbea43f63c6769
HyerimNac:/Users/나혜림/OneDrive/바탕 화면/test/venv/Scripts/python.exe <rimhyena@gmail.com>
2023-08-22 12:24:35 +0900

    ✨feat:통화방 modal 구현

--- src/components/dashboard/dialogbox/DiallogBox.tsx
+++ src/components/dashboard/dialogbox/DiallogBox.tsx
@@ -8,28 +8,60 @@ import * as S from "./style";
 import svgPath from "../../../assets/images/noticeModal_bang.svg";
 import facePath from "../../../assets/images/dissatisfied_Face.svg";
 import handPath from "../../../assets/images/sign_language.svg";
-import instance from "../../../api/api";
 
 const DiallogBox = () => {
   const navigate = useNavigate();
-  const onClickGotoCallRoom = () => {
+
+  const [isNoticeModalOpen, setIsNoticeModalOpen] = React.useState(false);
+
+  const onClickOpenNoticeModal = () => {
+    setIsNoticeModalOpen(true);
+  };
+
+  const onClickOpenMatchingModal = () => {
+    setIsNoticeModalOpen(false);
     navigate("/calling");
   };
 
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
-
-  const onClickOpenModal = () => {
-    setIsModalOpen(true);
-  };
-
-  const onClickcloseModal = () => {
-    setIsModalOpen(false);
-  };
-
-  console.log(isModalOpen, "isModalOpen");
+  console.log(isNoticeModalOpen, "isNoticeModalOpen");
 
   return (
     <>
+      {isNoticeModalOpen && (
+        <M.Wrap>
+          <M.Container>
+            <M.Icon>
+              <img style={{ width: "100%" }} src={svgPath} alt="bang" />
+            </M.Icon>
+            <h2>통화 전 한번만 읽어주세요!</h2>
+            <M.Content>
+              <h4>처음 만나는 친구와 대화하기 전</h4>
+              <h4>몇가지만 기억해주세요!</h4>
+            </M.Content>
+            <M.NoticeContainer>
+              <M.Notice>
+                <M.Icon size="small">
+                  <img style={{ width: "48px", height: "48px" }} src={facePath} alt="face" />
+                </M.Icon>
+                <p>
+                  상대방에 대한 예의를 지켜주세요. 공격적인 언행 및 차별적인 발언은 신고의 대상이 될
+                  수 있습니다.
+                </p>
+              </M.Notice>
+              <M.Notice>
+                <M.Icon size="small">
+                  <img style={{ width: "48px", height: "48px" }} src={handPath} alt="hand" />
+                </M.Icon>
+                <p>
+                  통화를 종료할때는 매너있게 나간다는 인사 후 통화를 종료해주세요. 그냥 대화방을
+                  나갈 시 상대방이 당황스러울수 있습니다.
+                </p>
+              </M.Notice>
+            </M.NoticeContainer>
+            <M.StartButton onClick={onClickOpenMatchingModal}>통화 시작하기</M.StartButton>
+          </M.Container>
+        </M.Wrap>
+      )}
       <DashBoardBox style={dialogBoxStyle} size="diallogBox">
         <S.ImageContainer>
           <img src={require("../../../assets/images/dialogbox.png")} alt="dailogbox" />
@@ -38,60 +70,9 @@ const DiallogBox = () => {
               반가워요!<br></br>스트로베리님!
             </div>
             <S.ButtonWrapper>
-              <>
-                <Button.Primary style={diallogBtnStyle} onClick={onClickOpenModal}>
-                  통화하기
-                </Button.Primary>
-                {isModalOpen && (
-                  <M.Wrap>
-                    {/* 모달 내무 컨테이너 */}
-                    <M.Container>
-                      {/* 아이콘 */}
-                      <M.Icon>
-                        <img style={{ width: "100%" }} src={svgPath} alt="bang" />
-                      </M.Icon>
-                      {/* 타이틀 */}
-                      <h2>통화 전 한번만 읽어주세요!</h2>
-                      {/* 상세 내용 */}
-                      <M.Content>
-                        <h4>처음 만나는 친구와 대화하기 전</h4>
-                        <h4>몇가지만 기억해주세요!</h4>
-                      </M.Content>
-                      {/* 안내문 */}
-                      <M.NoticeContainer>
-                        <M.Notice>
-                          <M.Icon size="small">
-                            <img
-                              style={{ width: "48px", height: "48px" }}
-                              src={facePath}
-                              alt="face"
-                            />
-                          </M.Icon>
-                          <p>
-                            상대방에 대한 예의를 지켜주세요. 공격적인 언행 및 차별적인 발언은 신고의
-                            대상이 될 수 있습니다.
-                          </p>
-                        </M.Notice>
-                        <M.Notice>
-                          <M.Icon size="small">
-                            <img
-                              style={{ width: "48px", height: "48px" }}
-                              src={handPath}
-                              alt="hand"
-                            />
-                          </M.Icon>
-                          <p>
-                            통화를 종료할때는 매너있게 나간다는 인사 후 통화를 종료해주세요. 그냥
-                            대화방을 나갈 시 상대방이 당황스러울수 있습니다.
-                          </p>
-                        </M.Notice>
-                      </M.NoticeContainer>
-                      {/* 시작하기 */}
-                      <M.StartButton onClick={onClickGotoCallRoom}>통화 시작하기</M.StartButton>
-                    </M.Container>
-                  </M.Wrap>
-                )}
-              </>
+              <Button.Primary style={diallogBtnStyle} onClick={onClickOpenNoticeModal}>
+                통화하기
+              </Button.Primary>
             </S.ButtonWrapper>
           </S.InnerGroup>
         </S.ImageContainer>
