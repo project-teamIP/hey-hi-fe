bb0fd62831418140b130cf20b701275d9a86459c
HyerimNac:/Users/나혜림/OneDrive/바탕 화면/test/venv/Scripts/python.exe <rimhyena@gmail.com>
2023-08-23 02:09:30 +0900

    ♻️ refactor:  통화방 interest 컴포넌트 분리,코드 정리

---
+++ src/components/calling/interest/Style.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const CategoriContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-bottom: 22px;
+  gap: 13px;
+`;
+
+export const CategoriStyle = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 198px;
+  height: 101px;
+  border: none;
+  border-radius: 15px;
+  background: #ffffff;
+  cursor: pointer;
+`;
+
+export const Image = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+`;
+
+export const Name = styled.div`
+  margin-top: 10px;
+  font-size: 16px;
+  font-weight: 500;
+`;
