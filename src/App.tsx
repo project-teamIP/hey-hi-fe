6c3c2efec5c91f2c72ad4f07bda3c8c4a56649d6
2Zerozero <hawgae@gmail.com>
2023-08-29 02:03:46 +0900

    ✨ feat: 1440px 이하일때, PC지원만 한다는 페이지로 반응형 구현완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
 import Login from "./pages/login/Login";
@@ -12,25 +12,49 @@ import GlobalStyle from "./assets/styles/GlobalStyle";
 import KakaoRedirect from "./pages/login/KakaoRedirect";
 import GoogleRedirect from "./pages/login/GoogleRedirect";
 import Error from "./pages/Error";
+import Guide from "./pages/Guide";
 
 function App() {
+  // 반응형 Width 값
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const isSmallScreen = windowWidth <= 1440;
+
+  console.log(windowWidth);
   return (
     <div>
       <GlobalStyle />
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashboard" element={<DashBoard />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/review" element={<Review />} />
-          <Route path="/calling" element={<Calling />} />
-          <Route path="/oauth/kakao" element={<KakaoRedirect />} />
-          <Route path="/oauth/google" element={<GoogleRedirect />} />
-          <Route path="*" element={<Error />} />
-        </Routes>
+        {isSmallScreen ? (
+          <Guide />
+        ) : (
+          <>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Main />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/dashboard" element={<DashBoard />} />
+              <Route path="/mypage" element={<MyPage />} />
+              <Route path="/review" element={<Review />} />
+              <Route path="/calling" element={<Calling />} />
+              <Route path="/oauth/kakao" element={<KakaoRedirect />} />
+              <Route path="/oauth/google" element={<GoogleRedirect />} />
+              <Route path="*" element={<Error />} />
+            </Routes>
+          </>
+        )}
       </BrowserRouter>
     </div>
   );
